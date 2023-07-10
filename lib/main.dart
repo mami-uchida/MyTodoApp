7d9f23c31ff7831a4278d7814e3937edd6ce2211
uchida <mamimami49192@gmail.com>
2023-07-10 16:27:58 +0900

    TODOの編集機能を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 
-
 void main() {
   // 最初に表示するWidget
-  runApp(MyTodoApp());
+  runApp(const MyTodoApp());
 }
 
 class MyTodoApp extends StatelessWidget {
+  const MyTodoApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,13 +22,15 @@ class MyTodoApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       // リスト一覧画面を表示
-      home: TodoListPage(),
+      home: const TodoListPage(),
     );
   }
 }
 
 // リスト一覧画面用Widget
 class TodoListPage extends StatefulWidget {
+  const TodoListPage({super.key});
+
   @override
   _TodoListPageState createState() => _TodoListPageState();
 }
@@ -50,8 +53,31 @@ class _TodoListPageState extends State<TodoListPage> {
           return Slidable(
             endActionPane: ActionPane(
               motion: const StretchMotion(),
-              extentRatio: 0.20,
+              extentRatio: 0.30,
               children: [
+                SlidableAction(
+                  onPressed: ( _ ) async {
+                    // "push"で新規画面に遷移
+                    // リスト追加画面から渡される値を受け取る
+                    final editedText = await Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) {
+                      // 遷移先の画面としてリスト追加画面を指定
+                      return const TodoEditPage();
+                      }),
+                    );
+                    if (editedText != null) {
+                    // キャンセルした場合は newListText が null となるので注意
+                      setState(() {
+                        // 編集されたテキストをリストに反映
+                        todoList.add(editedText);
+                        todoList.removeAt(index);
+                      });
+                    }               
+                  },
+                backgroundColor: Colors.yellow,
+                  icon: Icons.edit,
+                  label: '編集',
+                ),
                 SlidableAction(
                   onPressed: ( _ ) {
                     showDialog(
@@ -107,7 +133,7 @@ class _TodoListPageState extends State<TodoListPage> {
           final newListText = await Navigator.of(context).push(
             MaterialPageRoute(builder: (context) {
               // 遷移先の画面としてリスト追加画面を指定
-              return TodoAddPage();
+              return const TodoAddPage();
             }),
           );
           if (newListText != null) {
@@ -124,7 +150,11 @@ class _TodoListPageState extends State<TodoListPage> {
   }
 }
 
+
+// 追加画面用Widget
 class TodoAddPage extends StatefulWidget {
+  const TodoAddPage({super.key});
+
   @override
   _TodoAddPageState createState() => _TodoAddPageState();
 }
@@ -172,6 +202,82 @@ class _TodoAddPageState extends State<TodoAddPage> {
                   Navigator.of(context).pop(_text);
                 },
                 child: const Text('リスト追加', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+            const SizedBox(height: 8),
+            Container(
+              // 横幅いっぱいに広げる
+              width: double.infinity,
+              // キャンセルボタン
+              child: TextButton(
+                // ボタンをクリックした時の処理
+                onPressed: () {
+                  // "pop"で前の画面に戻る
+                  Navigator.of(context).pop();
+                },
+                child: const Text('キャンセル'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+// 編集画面用Widget
+class TodoEditPage extends StatefulWidget {
+  const TodoEditPage({super.key});
+
+  @override
+  _TodoEditPageState createState() => _TodoEditPageState();
+}
+
+class _TodoEditPageState extends State<TodoEditPage> {
+  // 入力されたテキストをデータとして持つ
+  String _text = '';
+
+  // データを元に表示するWidget
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('リスト編集'),
+      ),
+      body: Container(
+        // 余白を付ける
+        padding: const EdgeInsets.all(64),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            // 入力されたテキストを表示
+            Text(_text, style: const TextStyle(color: Colors.blue)),
+            const SizedBox(height: 8),
+            // テキスト入力
+            TextField(
+              // 入力されたテキストの値を受け取る（valueが入力されたテキスト）
+              onChanged: (String value) {
+                // データが変更したことを知らせる（画面を更新する）
+                setState(() {
+                  // データを変更
+                  _text = value;
+                });
+              },
+            ),
+            const SizedBox(height: 8),
+            Container(
+              // 横幅いっぱいに広げる
+              width: double.infinity,
+              // リスト追加ボタン
+              child: ElevatedButton(
+                onPressed: () {
+                  // "pop"で前の画面に戻る
+                  // "pop"の引数から前の画面にデータを渡す
+                  Navigator.of(context).pop(_text);
+                },
+                child: const Text('編集する', style: TextStyle(color: Colors.white)),
               ),
             ),
             const SizedBox(height: 8),
